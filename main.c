b21b2191c56f61d6f125a168cf8e056488f08577
liuphillip <38872415+Liuphillip@users.noreply.github.com>
2026-01-07 14:56:46 -0800

    uploaded main for the first time

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+	srand(time(NULL));
+    
+    
+    int randomNumber = rand() % 21;
+    int guess = 0;
+    int maxTries = 5;
+    
+    do{
+        printf("guess a number: ");
+        scanf("%d", &guess);
+        
+        if (guess > randomNumber){
+            printf("too high\n");
+        }
+        else if (guess < randomNumber){
+            printf("too low\n");
+        }
+        else{
+            printf("you got it.");
+            break;
+        }
+        maxTries--;
+        
+    } while(guess != randomNumber || maxTries==0);
+    
+	return 0;
+}
